18f850de697a8d355f9d592aa5371fdc1a8438f2
april0114 <sue5520607@skuniv.ac.kr>
2025-04-15 23:21:44 -0700

    🐛 Fix: 버그 수정

--- mallapi/extern/yck/dev/mock.php
+++ mallapi/extern/yck/dev/mock.php
@@ -6,6 +6,5 @@ $input = json_decode(file_get_contents("php://input"), true);
 file_put_contents(__DIR__ . '/mock_log.txt', print_r($input, true), FILE_APPEND);
 
 echo json_encode([
-    "result" => 0,
-    "message" => "External API simulated response"
+    "result" => 0,외부 API simulated response"
 ]);
